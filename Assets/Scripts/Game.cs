7895ca812cf8fe41b45cd5fb276cae64ac6f5111
Gijs Schouten <21969@ma-web.nl>
2020-03-11 10:00:50 +0100

    game, player, effects

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+	private int _playerCount;
+	private int _currentPlayer;
+
+	[SerializeField]
+	private GameObject[] _players;
+	public GameObject player;
+
+	public GameObject[] tiles;
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+
+	private void StartTurn() {
+		_currentPlayer = (_currentPlayer % _playerCount) + 1;
+		player = _players[_currentPlayer - 1];
+		player.GetComponent<Player>().StartTurn();
+	}
+
+	private void EndTurn() {
+
+	}
+
+}
