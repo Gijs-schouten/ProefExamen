6de83b43eb931cc57d7f5ecfa71351d97f0ef364
22092@ma-web.nl <22092@ma-web.nl>
2020-03-12 11:59:38 +0100

    prototype playermovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public  GameObject player;
-    public GameObject _nextSpace;
+    public GameObject player;
+    private GameObject _nextSpace;
     public Animator anim;
-
+    private int _currentSpace = 0;
+    private Game _game;
+    private GameObject[] _spaces;
     private float _elapsedTime;
     private float _waitTime = 1.5f;
     private Vector3 _nextSpaceVector;
     private int _diceRoll;
     private bool _isMoving = false;
-    private int sharting;
+    private int _stepCounter;
 
     void Start()
     {
         _diceRoll = 4;
+        _game = GetComponent<Game>();
+        _spaces = _game.tiles;        
         anim.GetComponent<Animator>();
-        _nextSpaceVector = new Vector3(_nextSpace.transform.position.x, _nextSpace.transform.position.y + .5f, _nextSpace.transform.position.z);
     }
 
     void Update()
@@ -32,11 +35,13 @@ public class PlayerMovement : MonoBehaviour
 
     void GoToNextSpace()
     {
-       if(sharting <= _diceRoll && !_isMoving)
+       if(_stepCounter <= _diceRoll && !_isMoving)
         {
-                sharting += 1;
+                _stepCounter += 1;
                 anim.SetBool("isJumping", true);
-                StartCoroutine(MoveToNextSpace());
+            _nextSpace = _spaces[_currentSpace +=1];
+            _nextSpaceVector = new Vector3(_nextSpace.transform.position.x, _nextSpace.transform.position.y + .5f, _nextSpace.transform.position.z);
+            StartCoroutine(MoveToNextSpace());
         }
     }
 
